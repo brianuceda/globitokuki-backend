9a0d10a6a0a59b4178129c459e50d53c96e4bade
Brian Uceda <monalek22@gmail.com>
2024-07-23 03:15:20 -0500

    feat(dev): base project

---
+++ src/main/java/site/globitokuki/globitokuki_backend/GlobitokukiBackendApplication.java
@@ -0,0 +1,13 @@
+package site.globitokuki.globitokuki_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobitokukiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobitokukiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/globitokuki/globitokuki_backend/GlobitokukiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package site.globitokuki.globitokuki_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobitokukiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
