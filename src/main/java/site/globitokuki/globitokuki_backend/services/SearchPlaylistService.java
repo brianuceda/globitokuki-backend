b5836959f1056486fc1e7e7b2eed00ecf54a4776
Brian Uceda <monalek22@gmail.com>
2024-08-12 09:04:12 -0500

    fix(jsoup): remove only services from docker-compose

--- src/main/java/site/globitokuki/globitokuki_backend/services/SearchPlaylistService.java
+++ src/main/java/site/globitokuki/globitokuki_backend/services/SearchPlaylistService.java
@@ -1,13 +1,13 @@
-// package site.globitokuki.globitokuki_backend.services;
+package site.globitokuki.globitokuki_backend.services;
 
-// import java.util.List;
+import java.util.List;
 
-// import site.globitokuki.globitokuki_backend.dtos.PlaylistDTO;
-// import site.globitokuki.globitokuki_backend.dtos.selenium.CookiesDTO;
+import site.globitokuki.globitokuki_backend.dtos.PlaylistDTO;
+import site.globitokuki.globitokuki_backend.dtos.selenium.CookiesDTO;
 
-// public interface SearchPlaylistService {
-//   void saveCookiesInDb(List<CookiesDTO> cookies);
-//   // void searchGoogle(String playlistYtUrl);
-//   List<String> searchGoogleImages(String query);
-//   PlaylistDTO searchYtPlaylistData(String playlistYtUrl);
-// }
+public interface SearchPlaylistService {
+  void saveCookiesInDb(List<CookiesDTO> cookies);
+  void searchGoogle(String playlistYtUrl);
+  List<String> searchGoogleImages(String query);
+  PlaylistDTO searchYtPlaylistData(String playlistYtUrl);
+}
