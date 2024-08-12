30ec4489ff85f4b127f3d250f03c11465545d92f
Brian Uceda <monalek22@gmail.com>
2024-08-12 08:44:17 -0500

    fix(dev): remove more unused code

--- src/main/java/site/globitokuki/globitokuki_backend/configs/CacheConfig.java
+++ src/main/java/site/globitokuki/globitokuki_backend/configs/CacheConfig.java
@@ -1,41 +1,41 @@
-package site.globitokuki.globitokuki_backend.configs;
+// package site.globitokuki.globitokuki_backend.configs;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheConfiguration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisPassword;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.cache.CacheManager;
+// import org.springframework.cache.annotation.EnableCaching;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.data.redis.cache.RedisCacheConfiguration;
+// import org.springframework.data.redis.cache.RedisCacheManager;
+// import org.springframework.data.redis.connection.RedisConnectionFactory;
+// import org.springframework.data.redis.connection.RedisPassword;
+// import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+// import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
-@Configuration
-@EnableCaching
-public class CacheConfig {
-  @Value("${REDIS_HOST}")
-  private String redisHost;
-  @Value("${REDIS_PASSWORD}")
-  private String redisPassword;
-  @Value("${REDIS_PORT}")
-  private int redisPort;
+// @Configuration
+// @EnableCaching
+// public class CacheConfig {
+//   @Value("${REDIS_HOST}")
+//   private String redisHost;
+//   @Value("${REDIS_PASSWORD}")
+//   private String redisPassword;
+//   @Value("${REDIS_PORT}")
+//   private int redisPort;
 
-  @Bean
-  public RedisConnectionFactory redisConnectionFactory() {
-    RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
-    redisConfig.setHostName(redisHost);
-    redisConfig.setPort(redisPort);
-    redisConfig.setPassword(RedisPassword.of(this.redisPassword));
-    return new LettuceConnectionFactory(redisConfig);
-  }
+//   @Bean
+//   public RedisConnectionFactory redisConnectionFactory() {
+//     RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
+//     redisConfig.setHostName(redisHost);
+//     redisConfig.setPort(redisPort);
+//     redisConfig.setPassword(RedisPassword.of(this.redisPassword));
+//     return new LettuceConnectionFactory(redisConfig);
+//   }
 
-  @Bean
-  public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
-    RedisCacheConfiguration defaultCacheConfig = RedisCacheConfiguration.defaultCacheConfig();
-    return RedisCacheManager.builder(redisConnectionFactory)
-      .cacheDefaults(defaultCacheConfig)
-      .build();
-  }
-}
+//   @Bean
+//   public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+//     RedisCacheConfiguration defaultCacheConfig = RedisCacheConfiguration.defaultCacheConfig();
+//     return RedisCacheManager.builder(redisConnectionFactory)
+//       .cacheDefaults(defaultCacheConfig)
+//       .build();
+//   }
+// }
--- src/main/java/site/globitokuki/globitokuki_backend/utils/SeleniumUtils.java
+++ src/main/java/site/globitokuki/globitokuki_backend/utils/SeleniumUtils.java
@@ -1,59 +1,59 @@
-package site.globitokuki.globitokuki_backend.utils;
+// package site.globitokuki.globitokuki_backend.utils;
 
-import java.net.URL;
-import java.time.Duration;
+// import java.net.URL;
+// import java.time.Duration;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+// import org.openqa.selenium.By;
+// import org.openqa.selenium.WebDriver;
+// import org.openqa.selenium.WebElement;
+// import org.openqa.selenium.chrome.ChromeDriver;
+// import org.openqa.selenium.chrome.ChromeOptions;
+// import org.openqa.selenium.remote.RemoteWebDriver;
+// import org.openqa.selenium.support.ui.ExpectedConditions;
+// import org.openqa.selenium.support.ui.WebDriverWait;
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.stereotype.Component;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+// import io.github.bonigarcia.wdm.WebDriverManager;
 
-@Component
-@SuppressWarnings("unused")
-public class SeleniumUtils {
-  @Value("${APP_PRODUCTION}")
-  private boolean isProduction;
-  public final static Duration TIMEOUT = Duration.ofSeconds(5);
+// @Component
+// @SuppressWarnings("unused")
+// public class SeleniumUtils {
+//   @Value("${APP_PRODUCTION}")
+//   private boolean isProduction;
+//   public final static Duration TIMEOUT = Duration.ofSeconds(5);
 
-  public ThreadLocal<RemoteWebDriver> setUp(ThreadLocal<RemoteWebDriver> driver) throws Exception {
-    ChromeOptions options = new ChromeOptions();
-    options.addArguments("--start-maximized"); // Maximizar ventana
-    options.addArguments("--disable-notifications"); // Desactivar notificaciones
+//   public ThreadLocal<RemoteWebDriver> setUp(ThreadLocal<RemoteWebDriver> driver) throws Exception {
+//     ChromeOptions options = new ChromeOptions();
+//     options.addArguments("--start-maximized"); // Maximizar ventana
+//     options.addArguments("--disable-notifications"); // Desactivar notificaciones
     
-    if (this.isProduction) {      
-      options.addArguments("--headless"); // Si estás corriendo en un entorno sin GUI
-      options.addArguments("--no-sandbox"); // Desactivar sandbox
-      options.addArguments("--disable-dev-shm-usage"); // Desactivar uso de memoria compartida
-      options.addArguments("--remote-allow-origins=*"); // Permitir orígenes remotos
+//     if (this.isProduction) {      
+//       options.addArguments("--headless"); // Si estás corriendo en un entorno sin GUI
+//       options.addArguments("--no-sandbox"); // Desactivar sandbox
+//       options.addArguments("--disable-dev-shm-usage"); // Desactivar uso de memoria compartida
+//       options.addArguments("--remote-allow-origins=*"); // Permitir orígenes remotos
 
-      // Configura la URL del Selenium Hub (cambia la URL si es necesario)
-      driver.set(new RemoteWebDriver(new URL("http://selenium-hub:4444"), options));
-    } else {
-      WebDriverManager.chromedriver().setup();
-      driver.set(new ChromeDriver(options));
-    }
+//       // Configura la URL del Selenium Hub (cambia la URL si es necesario)
+//       driver.set(new RemoteWebDriver(new URL("http://selenium-hub:4444"), options));
+//     } else {
+//       WebDriverManager.chromedriver().setup();
+//       driver.set(new ChromeDriver(options));
+//     }
 
-    return driver;
-  }
+//     return driver;
+//   }
 
-  private void closeBrowser(ThreadLocal<RemoteWebDriver> driver) {
-    driver.get().quit();
-    driver.remove();
-  }
+//   private void closeBrowser(ThreadLocal<RemoteWebDriver> driver) {
+//     driver.get().quit();
+//     driver.remove();
+//   }
   
-  // Espera hasta que el texto de un elemento con id específico cambie
-  public static void waitUntilTextChanges(WebDriver driver, By locator) {
-    WebDriverWait wait = new WebDriverWait(driver, SeleniumUtils.TIMEOUT);
-    WebElement element = driver.findElement(locator);
-    String initialText = element.getText().trim();
-    wait.until(ExpectedConditions.not(ExpectedConditions.textToBe(locator, initialText)));
-  }
-}
+//   // Espera hasta que el texto de un elemento con id específico cambie
+//   public static void waitUntilTextChanges(WebDriver driver, By locator) {
+//     WebDriverWait wait = new WebDriverWait(driver, SeleniumUtils.TIMEOUT);
+//     WebElement element = driver.findElement(locator);
+//     String initialText = element.getText().trim();
+//     wait.until(ExpectedConditions.not(ExpectedConditions.textToBe(locator, initialText)));
+//   }
+// }
